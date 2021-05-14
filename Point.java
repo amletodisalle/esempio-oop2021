b90cb7f0133993b8152230781cfeb1743120a9bc
amleto.disalle <amleto.disalle@univaq.it>
2021-05-14 11:40:01 +0200

    update

--- Point.java
+++ Point.java
@@ -4,8 +4,12 @@ public class Point {
 	
 	public void move(int x, int y) {
 			this.x = x;
-			this.y = y;
+		this.y = y;
 		System.out.println("metodo move");
+	}
+	
+	public String toString() {
+		return "Point:" + x + "," + y;
 	}
 	
 }
